1de0a5c3c8b0a2c47420e6b69508b72305ec8661
Ziko <manuelc79@gmail.com>
2024-10-13 18:51:56 -0300

    actualizacion de DatosPacientes

--- FrontEnd/app/components/DatosPaciente.tsx
+++ FrontEnd/app/components/DatosPaciente.tsx
@@ -9,7 +9,7 @@ export default function DatosPaciente({ paciente }: Props) {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Información del Paciente</h2>
-      <p><strong>Nombre:</strong> {paciente.nombre} {paciente.apellido}</p>
+      <p><strong>Nombre:</strong> {paciente.apellido} {paciente.nombre} </p>
       <p><strong>DNI:</strong> {paciente.dni}</p>
       <p><strong>Teléfono:</strong> {paciente.telefono}</p>
       <p><strong>Obra Social:</strong> {paciente.ObraSocial}</p>
