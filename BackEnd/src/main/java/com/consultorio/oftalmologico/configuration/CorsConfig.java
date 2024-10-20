a78ea503f9c3b073f6a6e535a7872d2d55caa185
Ziko <manuelc79@gmail.com>
2024-10-20 14:08:32 -0300

    Configuracion Cors Backend

--- BackEnd/src/main/java/com/consultorio/oftalmologico/configuration/CorsConfig.java
+++ BackEnd/src/main/java/com/consultorio/oftalmologico/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080/", "http://localhost:3000/", "https://localhost:8080/" )
+                        .allowedOrigins("https://opticonsult.onrender.com", "https://localhost:8080/" )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
